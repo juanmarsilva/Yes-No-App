61e895434b6a7affa4dc57e1cd97acf2dca587e2
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-06-03 02:02:13 -0300

    starting creating chat screen

---
+++ lib/presentation/chat/chat_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(6.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                'https://e1.pxfuel.com/desktop-wallpaper/35/259/desktop-wallpaper-anna-shumate-from-tiktok.jpg'),
+          ),
+        ),
+        title: const Text('Anna'),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return Text('Indice: $index');
+              },
+            )),
+          ],
+        ),
+      ),
+    );
+  }
+}
