be06f98634962acf39ced20de1847d0672593047
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-06-10 14:55:26 -0300

    change name of list

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(text: 'Hola Anna!', fromWho: FromWho.me),
     Message(text: 'Queres salir?', fromWho: FromWho.me),
   ];
