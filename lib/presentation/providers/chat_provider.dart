d77bcf761af0205170174241808e70278863d0a7
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-06-10 01:07:26 -0300

    create chat provider

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> message = [
+    Message(text: 'Hola Anna!', fromWho: FromWho.me),
+    Message(text: 'Queres salir?', fromWho: FromWho.me),
+  ];
+
+  Future<void> sendMessage() async {}
+}
