4241ebcf4e3f0f1e65d6d65bdaa0f352dafc502b
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-06-10 17:41:39 -0300

    generate http petition and the model of responses

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final GetYesNoAnswer getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: 'Hola Anna!', fromWho: FromWho.me),
     Message(text: 'Queres salir?', fromWho: FromWho.me),
   ];
+
+  Future<void> herRepply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom();
+  }
 
   Future<void> sendMessage(String text) async {
     if (text.isEmpty) return;
@@ -15,6 +24,10 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.me);
 
     messageList.add(newMessage);
+
+    if (text.endsWith('?')) {
+      await herRepply();
+    }
 
     /* 
       Con esto Flutter notifica a todos los lugares donde se este escuchando sobre este cambio, es similar al set  State  
