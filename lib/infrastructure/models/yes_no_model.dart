9ee29ce5c1bfcfb6cb964f73f4aeefb6e880a534
Juan Martin Silva <juanmartinsilva74@gmail.com>
2023-06-22 22:31:45 -0300

    create yes no model and use it

--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,7 +1,9 @@
+import 'package:yes_no_app/domain/entities/message.dart';
+
 class YesNoModel {
-  String answer;
-  bool forced;
-  String image;
+  final String answer;
+  final bool forced;
+  final String image;
 
   YesNoModel({
     required this.answer,
@@ -10,5 +12,19 @@ class YesNoModel {
   });
 
   factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
-      answer: json['answer'], forced: json['forced'], image: json['image']);
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+      };
+
+  Message toMessageEntity() => Message(
+      text: answer == 'yes' ? 'Si' : 'No',
+      fromWho: FromWho.hers,
+      imageUrl: image);
 }
